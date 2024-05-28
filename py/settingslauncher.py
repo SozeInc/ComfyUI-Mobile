67205f8714d0173fa9df9c51d718404c3bbb5523
Mike Rowley <m@soze.biz>
2024-05-28 16:20:56 -0600

    update

--- py/settingslauncher.py
+++ py/settingslauncher.py
@@ -246,15 +246,14 @@ class Settings_Launcher:
                 
             }
         }
-
         return inputs
 
-    RETURN_TYPES = (SettingsLauncherData)
-    RETURN_NAMES = ("settings_launcher_outputs")
+    RETURN_NAMES = (SettingsLauncherData)
+    RETURN_TYPES = ("settings_launcher_outputs")
     FUNCTION = "settings_launcher"
     CATEGORY = "Comfy Mobile"
 
-    def settings_launcher(self, seed,steps,cfg,width,height,batch_count,sampler_name,scheduler,denoise,custom_name_1, custom_type_1, custom_value_1, custom_name_2, custom_type_2, custom_value_2, custom_name_3, custom_type_3, custom_value_3, custom_name_4, custom_type_4, custom_value_4, custom_name_5, custom_type_5, custom_value_5, custom_name_6, custom_type_6, custom_value_6, custom_name_7, custom_type_7, custom_value_7, custom_name_8, custom_type_8, custom_value_8, custom_name_9, custom_type_9, custom_value_9, custom_name_10, custom_type_10, custom_value_10, custom_name_11, custom_type_11, custom_value_11, custom_name_12, custom_type_12, custom_value_12, custom_name_13, custom_type_13, custom_value_13, custom_name_14, custom_type_14, custom_value_14,  custom_name_15, custom_type_15, custom_value_15, custom_name_16, custom_type_16, custom_value_16, custom_name_17, custom_type_17, custom_value_17, custom_name_18, custom_type_18, custom_value_18, custom_name_19, custom_type_19, custom_value_19, custom_name_20, custom_type_20, custom_value_20, custom_name_21, custom_type_21, custom_value_21, custom_name_22, custom_type_22, custom_value_22, custom_name_23, custom_type_23, custom_value_23, custom_name_24, custom_type_24, custom_value_24, custom_name_25, custom_type_25, custom_value_25):
+    def settings_launcher(self,resume,seed,steps,cfg,width,height,batch_count,sampler_name,scheduler,denoise,custom_name_1, custom_type_1, custom_value_1, custom_name_2, custom_type_2, custom_value_2, custom_name_3, custom_type_3, custom_value_3, custom_name_4, custom_type_4, custom_value_4, custom_name_5, custom_type_5, custom_value_5, custom_name_6, custom_type_6, custom_value_6, custom_name_7, custom_type_7, custom_value_7, custom_name_8, custom_type_8, custom_value_8, custom_name_9, custom_type_9, custom_value_9, custom_name_10, custom_type_10, custom_value_10, custom_name_11, custom_type_11, custom_value_11, custom_name_12, custom_type_12, custom_value_12, custom_name_13, custom_type_13, custom_value_13, custom_name_14, custom_type_14, custom_value_14,  custom_name_15, custom_type_15, custom_value_15, custom_name_16, custom_type_16, custom_value_16, custom_name_17, custom_type_17, custom_value_17, custom_name_18, custom_type_18, custom_value_18, custom_name_19, custom_type_19, custom_value_19, custom_name_20, custom_type_20, custom_value_20, custom_name_21, custom_type_21, custom_value_21, custom_name_22, custom_type_22, custom_value_22, custom_name_23, custom_type_23, custom_value_23, custom_name_24, custom_type_24, custom_value_24, custom_name_25, custom_type_25, custom_value_25):
         
         output = SettingsLauncherData(seed, steps, cfg, width, height, batch_count, sampler_name, scheduler, denoise, 
                       custom_name_1, custom_type_1, custom_value_1, custom_name_2, custom_type_2, custom_value_2, 
@@ -276,28 +275,28 @@ class Settings_Launcher:
 
 
 
-    def get_settings():
+    # def get_settings():
 
-        # Replace with your actual API endpoint
-        url = "http://localhost:49163/GetSettings"
+    #     # Replace with your actual API endpoint
+    #     url = "http://localhost:49163/GetSettings"
 
-        # Replace with your actual file path
-        file_path = os.path.join(mobilenodes_dir, "token.txt")
+    #     # Replace with your actual file path
+    #     file_path = os.path.join(mobilenodes_dir, "token.txt")
 
-        # Open the file and read its contents
-        with open(file_path, "r") as file:
-            user_token = file.read()
+    #     # Open the file and read its contents
+    #     with open(file_path, "r") as file:
+    #         user_token = file.read()
 
-        # Make the GET request
-        response = requests.get(url, params={"userToken": user_token})
+    #     # Make the GET request
+    #     response = requests.get(url, params={"userToken": user_token})
 
-        # Check the response
-        if response.status_code == 200:
-            # If the request was successful, print the settings
-            print(response.json())
-        else:
-            # If the request failed, print the status code
-            print(f"Request failed with status code {response.status_code}")
+    #     # Check the response
+    #     if response.status_code == 200:
+    #         # If the request was successful, print the settings
+    #         print(response.json())
+    #     else:
+    #         # If the request failed, print the status code
+    #         print(f"Request failed with status code {response.status_code}")
 
 
 
